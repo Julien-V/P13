30cb826675d6f804c43a1178da349450ce0ee216
Julien Vassort <julien.vassort61@gmail.com>
2021-03-02 11:34:11 +0100

    auto close bootstrap alert, alert doesn't shift content anymore, console.log removed in profile.html
    * app_blog/static/js/app_blog.js edited
     - cleaning old code and moving it to add_headers()
     - close_bs_alert() added
     - auto_close_bs_alert() added
     - windows.onload = auto_close_bs_alert; added
    
    * app_blog/templates/base.html edited
     - messages are now included in a div (with fixed-top class)
    
    * app_blog/templates/profile.html edited
     - console.log removed
    
    * app_blog/static/css/style.css updated

--- app_blog/static/js/app_blog.js
+++ app_blog/static/js/app_blog.js
@@ -1,12 +1,30 @@
-var csrf = document.querySelector("input[name=csrfmiddlewaretoken]").value
-var headers = new Headers();
-headers.append('X-Requested-With', 'XMLHttpRequest');
-headers.append('X-CSRFToken', csrf)
+function add_headers() {
+  var csrf = document.querySelector("input[name=csrfmiddlewaretoken]").value;
+  var headers = new Headers();
+  headers.append('X-Requested-With', 'XMLHttpRequest');
+  headers.append('X-CSRFToken', csrf);
+  return headers;
+}
+
 
 window.POST = function(url, data) {
-  return fetch(url, {method: "POST", headers: headers, body: JSON.stringify(data)});
+  return fetch(url, {method: "POST", headers: add_headers(), body: JSON.stringify(data)});
 }
 
 window.GET = function(url) {
 	return fetch(url)
 }
+
+function close_bs_alert() {
+  var alertNode = document.querySelectorAll('.alert');
+  alertNode.forEach(function(alertElem) {
+    let alert = new bootstrap.Alert(alertElem);
+    alert.close();
+  })
+}
+
+function auto_close_bs_alert() {
+  setTimeout(close_bs_alert, 3000);
+}
+
+window.onload = auto_close_bs_alert;
